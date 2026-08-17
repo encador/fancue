6c9ae9b95e68b32428f31fa5e2effa95fd3b3f5e
encador <encador@tuta.com>
2026-08-17 10:21:33 -0400

    Add Basic Server

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+type config struct {
+	address string
+	port    int
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	cnf := config{}
+	flag.StringVar(&cnf.address, "address", "localhost", "IP-address on which the application runs")
+	flag.IntVar(&cnf.port, "port", 8080, "Port on which the application runs")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	srv := http.Server{
+		Addr:    fmt.Sprintf("%s:%d", cnf.address, cnf.port),
+		Handler: mux,
+	}
+
+	go func() {
+		fmt.Println("[LOG] Serving on " + srv.Addr)
+		srv.ListenAndServe()
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	fmt.Println("\nShutting Down...")
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Shutdown Complete")
+
 }
